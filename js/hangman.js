6f295f75762ce24152d58f6d0ebafb14e7b64a12
Olly <ollydutton@hotmail.com>
2014-04-06 17:56:01 +1000

    js and css updates

--- js/hangman.js
+++ js/hangman.js
@@ -1,60 +1,100 @@
 $(document).ready(function() {
-
-  var words = ["word", "anotherword", "fart", "anotherfart"];
-
-  var word = ['a','p','p','l','e']   //_.sample(words).split('');
-  var underscores = [];
-  var guessedLetters = [];
-  var counter = 8;
-
-  //   var wordLength = word.length;
-  //     for ( i = 0; i < wordLength; i++) {
-  //     underscores = underscores + "_ ";
-  //     // new_solution = underscores.slice();
-  //     };
-  //   $("#solution").text(underscores);
-
-  var guess = function (letter) {
-
-    var solution = [];
-
-    guessedLetters.push(letter);
-
-    _.each(word, function(letter){    
-      if (_.indexOf(guessedLetters, letter) === -1) {
-        solution.push("_"); 
-        $('#solution').text(solution.join(' '));
-        incorrectGuess();
-      } else {
-        solution.push(letter); 
-        $('#solution').text(solution.join(' '));
-        // if (word. === solution) {
-        //    win();
-        // };
-      };
-    });
-  };
-
-  var incorrectGuess = function (letter) {
-    counter -= 1;
-    if (counter === 0) {
-      // lose();
-    };
-  };  
-
-  var lose = function () {
-    console.log('you suck!');
-  };
- 
+  // This sets the first image of gallows when the screen is loaded.
+    $("#gallows").append("<img id='gallows_img' src='img/img8.jpg'>")
+    
+  // These are the variables we use throughout the function
+    var words = ["word", "anotherword", "fart", "anotherfart"];
+    var word = ['w', 'o', 'r', 'd']          // _.sample(words).split('');
+    var underscores = [];
+    var guessedLetters = [];
+    var counter = 8;
   
-  var win = function () {
-    console.log('fuck yeah!');
-  };
+    var guess = function (letter) {
 
-  $("#letter-button").on('click', function(){
-    var letter = $('#letter-input').val();
-    // debugger;
-    guess(letter);
-  });
+      var solution = [];
 
+      guessedLetters.push(letter);
+
+      _.each(word, function(letter){    
+        if (_.indexOf(guessedLetters, letter) === -1) {
+          solution.push("_"); 
+          $('#solution').text(solution.join(' '));
+        } else {
+          solution.push(letter); 
+          $('#solution').text(solution.join(' '));
+          if (word.join('') === solution.join('')) {
+             win();
+          };
+        };
+      });
+    incorrectGuess(letter);
+    };
+
+   var incorrectGuess = function (letter) {
+      if (_.indexOf(word, letter) === -1) {
+        counter -= 1;
+        $("#guesses").text(counter);
+        $("#gallows").html("<img id='gallows_img' src='img/img" + counter + ".jpg'>")
+        console.log(counter);
+        if (counter === 0) {
+          $("#guess_inputs").hide();
+          lose();
+        };
+      };
+    };
+    
+
+    var lose = function () {
+    $("*")
+        .fadeOut(4000).delay(4000).queue(function() {
+        location.reload();
+    });
+
+      // location.reload();
+    };
+   
+    
+    var win = function () {
+
+      $("#gallows").fadeOut(1000);
+      $("#content").text('You Win!')
+      $("#content").css('color', 'white');
+      $("#content").css('font-size', '100px')
+      $('#content').each(function() {
+        var elem = $(this);
+          setInterval(function() {
+            if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible');
+            } else {
+            elem.css('visibility', 'hidden');
+            }    
+          }, 500);
+        });
+      $('body').css('background-image', 'url("img/psybackground.jpg")');
+      $("#reset-button").text('Play Again!');
+      $("#gif").css('img', 'url("img/psybackground.jpg")');
+      $('#cat-gif').each(function() {
+        var elem = $(this);
+          setInterval(function() {
+            if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible'); }  
+          });
+        });
+      $("#cat-gif").animate({left: "+1300"}, 5000);
+      $("#cat-gif").animate({left: "-800"}, 5000);
+    };
+
+    $("#giveup-button").on('click', function() {
+      $('#solution').text(word.join(''));
+    });
+
+    $("#reset-button").on('click', function() {
+      location.reload();
+    });
+
+    $("#letter-button").on('click', function(){
+      var letter = $('#letter-input').val();
+      guess(letter);
+    });
 });
+
